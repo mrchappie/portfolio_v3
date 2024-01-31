f5975480e9c7620b155233868986706cd0e3cd91
Alexandru BOSCU <boscu20@gmail.com>
2024-01-31 21:19:39 +0200

    small fixes

--- components/footer/footerComponent.js
+++ components/footer/footerComponent.js
@@ -10,15 +10,15 @@ const footerComponent = () => {
     <footer className={styles.footer}>
       <section className={styles.footer__contact}>
         <LogoComponent
-          style={{ '-webkit-text-fill-color': '#42446e' }}
+          style={{ '-webkit-text-fill-color': '#a7a7a7' }}
         ></LogoComponent>
-        <div className={styles.contact__options} style={{ color: '#42446e' }}>
+        <div className={styles.contact__options} style={{ color: '#a7a7a7' }}>
           <div className={styles.contact__options__phone}>+40771558180</div>
           <div className={styles.contact__options__email}>
             boscu20@gmail.com
           </div>
         </div>
-        <SocialLinks style={{ fill: '#42446e' }}></SocialLinks>
+        <SocialLinks style={{ fill: '#a7a7a7' }}></SocialLinks>
       </section>
       <div className={styles.divider}></div>
       <section className={styles.footer__navigation}>
